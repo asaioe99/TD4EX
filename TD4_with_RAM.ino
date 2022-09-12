5efb703b5ecf029ec70a18ee4666e92c3927edca
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-09-13 06:14:57 +0900

    Update TD4_with_RAM.ino

--- TD4_with_RAM.ino
+++ TD4_with_RAM.ino
@@ -23,30 +23,32 @@
 #define IN_B_Im  0b0110
 #define MOV_B_Im 0b0111
 #define PUSH_A   0b1000
-#define OUT_Im_A 0b1001
+#define OUT_A_Im 0b1001
 #define POP_A    0b1010
-#define OUT_A_Im 0b1011
+#define OUT_Im_A 0b1011
 #define ADDS     0b1100
 #define JC_Im    0b1101
 #define JNC_Im   0b1110
 #define JMP_Im   0b1111
 
 //ROMRAMレジスタ等
-byte rom[16];
-byte ram[16];
-byte c_flag = 0;
-byte reg_a  = 0b0000;
-byte reg_b  = 0b0000;
-byte reg_sp = 0b1111;
-byte reg_pc = 0b0000;
+uint8_t rom[16];
+uint8_t ram[16];
+uint8_t c_flag = 0;
+uint8_t reg_a  = 0b0000;
+uint8_t reg_b  = 0b0000;
+uint8_t reg_sp = 0b1111;
+uint8_t reg_pc = 0b0000;
 //実行ステップ数（スコア）
-unsigned long score = 0;
+uint32_t  score = 0;
 //命令フラグ
-int opcode;
+int32_t  opcode;
+//実行速度設定用
+uint8_t speed;
 
 LiquidCrystal_I2C lcd(0x27, 20, 04);
 
-void rom_select(int a) {
+void rom_select(int32_t a) {
   //アドレスaを入力する。
   digitalWrite(ROM_IN_0, (a & 0b1000) >> 3);
   digitalWrite(ROM_IN_1, (a & 0b0100) >> 2);
@@ -55,7 +57,7 @@ void rom_select(int a) {
 }
 //ROMの読み出し。グローバル変数に格納
 void rom_read() {
-  for (int i = 0b0000; i <= 0b1111; i++) {
+  for (int32_t i = 0b0000; i <= 0b1111; i++) {
     rom_select(i);
     rom[i] = 0;
     rom[i] += digitalRead(ROM_OUT_0) << 0;
@@ -70,13 +72,13 @@ void rom_read() {
 }
 
 void ram_init() {
-  for (int i = 0; i < 16; i++) {
+  for (int32_t i = 0; i < 16; i++) {
     ram[i] = 0b0000;
   }
 }
 
 void add_a_im() {
-  int tmp = rom[reg_pc] & 0b00001111;
+  int32_t tmp = rom[reg_pc] & 0b00001111;
   reg_a += tmp;
   if (reg_a > 0b1111) {
     c_flag = 1;
@@ -87,14 +89,16 @@ void add_a_im() {
   reg_pc = (++reg_pc) % 16;
 }
 void mov_a_b() {
-  int tmp = rom[reg_pc] & 0b00001111;
+  int32_t tmp = rom[reg_pc] & 0b00001111;
   reg_a = reg_b + tmp;
   c_flag = 0;
+  reg_a = reg_a % 16;
   reg_pc = (++reg_pc) % 16;
 }
 void in_a_im() {
   reg_a = ram[rom[reg_pc] & 0b00001111];
   c_flag = 0;
+  reg_a = reg_a % 16;
   reg_pc = (++reg_pc) % 16;
 }
 void mov_a_im() {
@@ -103,12 +107,14 @@ void mov_a_im() {
   reg_pc = (++reg_pc) % 16;
 }
 void mov_b_a() {
-  reg_b = reg_a;
+  int32_t tmp = rom[reg_pc] & 0b00001111;
+  reg_b = reg_a + tmp;
   c_flag = 0;
+  reg_b = reg_b % 16;
   reg_pc = (++reg_pc) % 16;
 }
 void add_b_im() {
-  int tmp = rom[reg_pc] & 0b00001111;
+  int32_t tmp = rom[reg_pc] & 0b00001111;
   reg_b += tmp;
   if (reg_b > 0b1111) {
     c_flag = 1;
@@ -121,6 +127,7 @@ void add_b_im() {
 void in_b_im() {
   reg_b = ram[rom[reg_pc] & 0b00001111];
   c_flag = 0;
+  reg_b = reg_b % 16;
   reg_pc = (++reg_pc) % 16;
 }
 void mov_b_im() {
@@ -129,7 +136,8 @@ void mov_b_im() {
   reg_pc = (++reg_pc) % 16;
 }
 void push_a() {
-  ram[reg_sp] = reg_a;
+  int32_t tmp = rom[reg_pc] & 0b00001111;
+  ram[reg_sp] = reg_a + tmp;
   if (reg_sp > 0) {
     reg_sp--;
   } else {
@@ -138,24 +146,27 @@ void push_a() {
   c_flag = 0;
   reg_pc = (++reg_pc) % 16;
 }
-void out_im_a() {
+void out_a_im() {
   ram[reg_a] = rom[reg_pc] & 0b00001111;
   c_flag = 0;
   reg_pc = (++reg_pc) % 16;
 }
 void pop_a() {
+  int32_t tmp = rom[reg_pc] & 0b00001111;
   reg_sp = (++reg_sp) % 16;
-  reg_a = ram[reg_sp];
+  reg_a = ram[reg_sp] + tmp;
   c_flag = 0;
   reg_pc = (++reg_pc) % 16;
 }
-void out_a_im() {
+void out_im_a() {
   ram[rom[reg_pc] & 0b00001111] = reg_a;
   c_flag = 0;
   reg_pc = (++reg_pc) % 16;
 }
 void adds() {
-  ram[(reg_sp + 2) % 16] += ram[(reg_sp + 1) % 16];
+  int32_t tmp = rom[reg_pc] & 0b00001111;
+  ram[(reg_sp + 2) % 16] += ram[(reg_sp + 1) % 16] + tmp;
+  ram[(reg_sp + 2) % 16] = ram[(reg_sp + 2) % 16] % 16;
   reg_sp = (++reg_sp) % 16;
   c_flag = 0;
   reg_pc = (++reg_pc) % 16;
@@ -192,59 +203,59 @@ void lcd_display() {
   lcd.setCursor(1, 3);
   switch (opcode) {
     case 0:
-      snprintf(pOPscr, 20, "%X:ADD  A ,  %X %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:ADD  A ,  %X %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 1:
-      snprintf(pOPscr, 20, "%X:MOV  A ,  B %05d", reg_pc, score);
+      snprintf(pOPscr, 20, "%X:MOV  A ,  B %05lu", reg_pc, score);
       break;
     case 2:
-      snprintf(pOPscr, 20, "%X:IN   A ,[0%X]%05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:IN   A ,[0%X]%05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       f_ram_cng = 1;
       break;
     case 3:
-      snprintf(pOPscr, 20, "%X:MOV  A ,  %X %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:MOV  A ,  %X %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 4:
-      snprintf(pOPscr, 20, "%X:MOV  B ,  A %05d", reg_pc, score);
+      snprintf(pOPscr, 20, "%X:MOV  B ,  A %05lu", reg_pc, score);
       break;
     case 5:
-      snprintf(pOPscr, 20, "%X:ADD  B ,  %X %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:ADD  B ,  %X %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 6:
-      snprintf(pOPscr, 20, "%X:IN   B ,[0%X]%05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:IN   B ,[0%X]%05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       f_ram_cng = 1;
       break;
     case 7:
-      snprintf(pOPscr, 20, "%X:MOV  B ,  %X %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:MOV  B ,  %X %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 8:
-      snprintf(pOPscr, 20, "%X:PUSH A      %05d", reg_pc, score);
+      snprintf(pOPscr, 20, "%X:PUSH A      %05lu", reg_pc, score);
       f_ram_cng = 1;
       break;
     case 9:
-      snprintf(pOPscr, 20, "%X:OUT  %X ,[ A]%05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:OUT  A ,[0%X]%05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       f_ram_cng = 1;
       break;
     case 10:
-      snprintf(pOPscr, 20, "%X:POP  A      %05d", reg_pc, score);
+      snprintf(pOPscr, 20, "%X:POP  A      %05lu", reg_pc, score);
       f_ram_cng = 1;
       break;
     case 11:
-      snprintf(pOPscr, 20, "%X:OUT  A ,[0%X]%05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:OUT  %X ,[ A]%05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       f_ram_cng = 1;
       break;
     case 12:
-      snprintf(pOPscr, 20, "%X:ADDS        %05d", reg_pc, score);
+      snprintf(pOPscr, 20, "%X:ADDS        %05lu", reg_pc, score);
       f_ram_cng = 1;
       break;
     case 13:
-      snprintf(pOPscr, 20, "%X:JC   %X      %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:JC   %X      %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 14:
-      snprintf(pOPscr, 20, "%X:JNC  %X      %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:JNC  %X      %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     case 15:
-      snprintf(pOPscr, 20, "%X:JMP  %X      %05d", reg_pc, rom[reg_pc] & 0b00001111, score);
+      snprintf(pOPscr, 20, "%X:JMP  %X      %05lu", reg_pc, rom[reg_pc] & 0b00001111, score);
       break;
     default:
       break;
@@ -266,7 +277,7 @@ void lcd_display() {
 }
 
 void serial_debug() {
-  for (int i = 0; i < 16; i++) {
+  for (int32_t i = 0; i < 16; i++) {
     Serial.print(i, HEX);
     Serial.print(":");
     Serial.print((rom[i] & 0b10000000) >> 7, BIN);
@@ -298,6 +309,9 @@ void setup() {
   pinMode(ROM_OUT_5, INPUT);
   pinMode(ROM_OUT_6, INPUT);
   pinMode(ROM_OUT_7, INPUT);
+  // 実行速度設定用
+  pinMode(14, INPUT);
+  speed = digitalRead(14);
 
   //LCD初期化
   lcd.init();
@@ -314,7 +328,14 @@ void setup() {
   lcd.print("C:");
   lcd.setCursor(0, 3);
   lcd.print("p");
-  lcd_display();
+  char RAM0_5[19];
+  char RAM6_B[19];
+  snprintf(RAM0_5, 17, "%X1:%X2:%X3:%X4:%X5:%X", ram[0x0], ram[0x1], ram[0x2], ram[0x3], ram[0x4], ram[0x5]);
+  snprintf(RAM6_B, 17, "%X7:%X8:%X9:%XA:%XB:%X", ram[0x6], ram[0x7], ram[0x8], ram[0x9], ram[0xA], ram[0xB]);
+  lcd.setCursor(2, 0);
+  lcd.print(RAM0_5);
+  lcd.setCursor(2, 1);
+  lcd.print(RAM6_B);
   //ROM読み込み
   rom_read();
   //RAM初期化
@@ -325,7 +346,6 @@ void setup() {
 
 void loop() {
   rom_select(reg_pc);
-
   switch (rom[reg_pc] >> 4) {
     case ADD_A_Im:
       opcode = 0;
@@ -372,20 +392,20 @@ void loop() {
       lcd_display();
       push_a();
       break;
-    case OUT_Im_A:
+    case OUT_A_Im:
       opcode = 9;
       lcd_display();
-      out_im_a();
+      out_a_im();
       break;
     case POP_A:
       opcode = 10;
       lcd_display();
       pop_a();
       break;
-    case OUT_A_Im:
+    case OUT_Im_A:
       opcode = 11;
       lcd_display();
-      out_a_im();
+      out_im_a();
       break;
     case ADDS:
       opcode = 12;
@@ -411,6 +431,9 @@ void loop() {
       break;
   }
   score++;
+  if (speed == 1) {
+    delay(1000);
+  }
   if (ram[0] > 3) {
     lcd_display();
     lcd.setCursor(19, 0);
